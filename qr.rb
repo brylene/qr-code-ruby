a1e6e04693d66e78e0b4f1792380bb52d4729daa
brylenelavelle Patrick <brylenelavelle@gmail.com>
2023-03-20 15:28:09 +0000

    QR Code Set Up

--- qr.rb
+++ qr.rb
@@ -1,1 +1,9 @@
-puts "hello world"
+require "rqrcode"
+
+# Use the RQRCode::QRCode class to encode some text
+qrcode = RQRCode::QRCode.new "WIFI:T:WPA;S:Brylene's Iphone;P:scaryteatree;;"
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
